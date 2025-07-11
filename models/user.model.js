2c63744c68bb3dcceee2db7567c1e0045a6ce366
Harshal <harshal.madgulkar725@gmail.com>
2025-07-11 17:43:26 +0530

    Fix type casing for schema fields in user model

--- models/user.model.js
+++ models/user.model.js
@@ -3,19 +3,19 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
 	{
 		name: {
-			type: string,
+			type: String,
 			required: true,
 		},
 		email: {
-			type: string,
+			type: String,
 			required: true,
 		},
 		password: {
-			type: string,
+			type: String,
 			required: true,
 		},
 		role: {
-			type: string,
+			type: String,
 			enum: ["instructor", "student"],
 			default: "student",
 		},
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema(
 			},
 		],
 		photoUrl: {
-			type: string,
+			type: String,
 			default: "",
 		},
 	},
